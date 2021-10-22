a7b7a8ce7fca6116ab70ef6312204a7fc163c1ef
Noah Belsito <32454695+noahbelsito@users.noreply.github.com>
2021-10-22 05:27:10 -0400

    billionaire model

---
+++ README.md
@@ -0,0 +1,2 @@
+# Billonaire First ML Model
+Predicts a billionaires probable worth using year founded, founder age, and the gdp of the area they're company is located.
---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+model_dataset = 'billionaires.csv'  # Set the file name
+billionaire_data = pd.read_csv(model_dataset)  # read csv into data
+billionaire_data = billionaire_data.dropna()  # drops N/A data
+y = billionaire_data['wealth.worth in billions']  # sets the value to predict
+features = ['company.founded', 'demographics.age', 'location.gdp']
+X = billionaire_data[features]  # features
+billionaire_model = DecisionTreeRegressor()  # define the model type to use
+billionaire_model.fit(X, y)
+predictions = billionaire_model.predict(X)
+mae = mean_absolute_error(y, predictions)
+print('Data MAE: ', mae)  # print mae
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)  # split the dataset
+billionaire_model.fit(train_X, train_y)  # fit features and value to predict to model type
+val_predictions = billionaire_model.predict(val_X)  # validate current model using validation data
+mae = mean_absolute_error(val_y, val_predictions)  # mae using val_y and val_predictions
+print('Split Data MAE: ', mae)  # print mae
